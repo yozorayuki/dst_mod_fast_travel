e55f5b2c47d712a28203edc1368a5dc480b8e6e2
WinterSky <sorayuki@foxmail.com>
2018-10-15 13:11:06 +0800

    pre version 1.04

--- modinfo.lua
+++ modinfo.lua
@@ -1,6 +1,6 @@
 name = "Fast Travel (with ui)"
 author = "SoraYuki"
-version = "1.03"
+version = "1.04"
 description = "version " .. version .. "\nBuild a fast travel network and travel instantly from sign post to sign post."
 
 forumthread = ""
